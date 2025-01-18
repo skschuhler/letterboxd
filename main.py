133d2ed101b4143b0b361498af36ee57fcdb598b
skschuhler <55286126+skschuhler@users.noreply.github.com>
2025-01-18 17:02:23 -0500

    add scraped popular users

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from utils import *
 # Running the asyncio event loop
 async def main():
     # Read the list of usernames from the CSV
-    users = pd.read_csv("C:/Users/sarah/Documents/GitHub/letterboxd-scraper/skipped_users.csv")['user'].to_list()
+    users = pd.read_csv("data/popular_users.csv")['user'].to_list()
 
     # Start scraping users concurrently
     await scrape_multiple_users_concurrently(users)
